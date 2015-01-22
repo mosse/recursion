994334a60a9bb197dcfb4e97c69122a400cab096
Mark Macdonald <mjemacdonald@gmail.com>
2015-01-22 12:31:27 -0800

    Getting going with stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  return stringify(obj);
+};
+
+var stringify = function(input) {
+	switch (typeof(input)){
+		case 'object':
+			return objectStringify(input);
+			break;
+		case 'number':
+			return input.toString();
+			break;
+		case 'string':
+			return '\"' + input + '\"';
+			break;
+		case 'boolean':
+			return input.toString();
+			break;
+		case 'undefined':
+			return null;
+			break;
+		default:
+			console.log('Why you no write something I understand?');
+	}
+};
+
+var objectStringify = function(objInput) {
+	if (objInput === null){
+		// Return 'null'
+	}
+	else if (Array.isArray(objInput)) {
+		return arrayStringify(objInput);
+	}
+	else {
+		// stringify object elements
+	}
+};
+
+var arrayStringify = function(arrInput) {
+    var result = "";
+	for (var i = 0; i < arrInput.length; i++){
+		result = result.concat(arrInput[i]);
+	}
+	return result;
 };
